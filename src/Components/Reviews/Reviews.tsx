13993bd65e470826e2c3229916184e56e6a9ad9b
vaaleerkiin <tahaburrent@gmail.com>
2023-10-21 20:48:24 +0200

    Update Reviews.tsx

--- src/Components/Reviews/Reviews.tsx
+++ src/Components/Reviews/Reviews.tsx
@@ -1,19 +1,7 @@
 import { IReviews } from "@/Types/IReviews";
 import dateFormat from "dateformat";
 
-import {
-  Container,
-  Heading,
-  Img,
-  Text,
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-  AccordionIcon,
-  Box,
-  Avatar,
-} from "@chakra-ui/react";
+import { Container, Text, Box, Avatar, VStack } from "@chakra-ui/react";
 import React from "react";
 
 export const Reviews = ({ reviews }: { reviews: IReviews[] }) => {
@@ -22,42 +10,39 @@ export const Reviews = ({ reviews }: { reviews: IReviews[] }) => {
       {reviews.length > 0 && (
         <Container
           marginTop={3}
+          marginBottom={3}
           maxW="4xl"
           backgroundColor="#f4a259"
           borderRadius={8}
+          paddingBottom={3}
         >
-          <Accordion defaultIndex={[0]} allowMultiple>
+          <VStack gap={6} paddingTop={4}>
             {reviews.map((el, ind) => (
-              <AccordionItem
+              <Box
                 key={el.id}
-                borderColor="black"
-                borderTop={`${ind === 0 && "none"}`}
+                borderTop={`${ind === 0 ? "none" : "1px solid black"}`}
+                paddingTop={`${ind === 0 ? 0 : 4}`}
               >
-                <AccordionButton>
-                  <Box as="span" flex="1" textAlign="left">
-                    <Avatar
-                      marginRight={2}
-                      size="xs"
-                      src={
-                        el.author_details.avatar_path
-                          ? `https://image.tmdb.org/t/p/w300/${el.author_details.avatar_path}?api_key=b3b4716df5187d0bc9138efc2668bc10`
-                          : `https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png`
-                      }
-                    />
-                    <Text display="inline"> {el.author}</Text>{" "}
-                    <Text display="inline" fontSize="xs">
-                      ({dateFormat(el.created_at, "mmmm dS, yyyy")})
-                    </Text>
-                  </Box>
-                  <AccordionIcon />
-                </AccordionButton>
+                <Box flex="1" textAlign="left">
+                  <Avatar
+                    marginRight={2}
+                    size="xs"
+                    src={
+                      el.author_details.avatar_path
+                        ? `https://image.tmdb.org/t/p/w300/${el.author_details.avatar_path}?api_key=b3b4716df5187d0bc9138efc2668bc10`
+                        : `https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png`
+                    }
+                  />
+                  <Text display="inline"> {el.author}</Text>{" "}
+                  <Text display="inline" fontSize="xs" marginLeft="auto">
+                    ({dateFormat(el.created_at, "mmmm dS, yyyy")})
+                  </Text>
+                </Box>
 
-                <AccordionPanel pb={4}>
-                  <Text>{el.content}</Text>
-                </AccordionPanel>
-              </AccordionItem>
+                <Text>{el.content}</Text>
+              </Box>
             ))}
-          </Accordion>
+          </VStack>
         </Container>
       )}
     </>
